edccacfaabd48fc7e70a147dbc82e57cd4ae5fd5
Igrey1321 <igrey1321@gmail.com>
2020-11-17 21:31:40 +0200

    add: owl carousel

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+  dots: true,
+  responsiveClass: true,
+  items: 1,
+  autoplay: true,
+  smartSpeed: 1000,
+  loop: true,
+  responsive: {
+    0: {
+
+    },
+    600: {
+
+    },
+    1000: {
+
+    }
+  }
+})
